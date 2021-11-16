1d8d4284aa21d1ad3faa3f438b14640546aa503e
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2021-11-16 20:54:41 +0100

    run db migration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id                   :bigint           not null, primary key
+#  application_deadline :datetime
+#  event_date           :datetime
+#  intro                :string
+#  name                 :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 class Event < ApplicationRecord
 end
